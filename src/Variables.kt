ee56471451a246660210ed4b77c55674680008a7
FilipeAlbuquerque <filipefragosoti@gmail.com>
2019-06-12 16:27:55 -0300

    Initial commit

---
+++ src/Variables.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    // variable "val" cannot be changed
+    val name = "Filipe Augusto"
+    var isAwesome = true
+
+    println("Is " + name + " awesome?" + "The answer is: " + isAwesome)
+
+    var doubleNum: Double = 2.34
+
+    var longNum: Long = 234L
+
+    var hero: String
+    hero = "batman"
+    hero = "superman"
+    println(hero)
+}
