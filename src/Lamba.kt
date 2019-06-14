bcaf707c44232961b0e9638362330421c9c4d350
FilipeAlbuquerque <filipefragosoti@gmail.com>
2019-06-14 19:28:54 -0300

    Implementing others requests with lambda

--- src/Lamba.kt
+++ src/Lamba.kt
@@ -11,7 +11,7 @@ fun main() {
     val sumB : (Int, Int) -> Int = {x,y -> x + y}
     println(sumB(5,4))
 
-
+    //working with web request examples
     fun downloadData(url: String, completion: ()-> Unit){
         //sent a download request
         //we got back data
@@ -25,5 +25,49 @@ fun main() {
                 " after the completion()")
     }
 
-    
+    fun downloadCarData(url: String, completion: (Car) -> Unit){
+        //sent a request
+        //we got back a car data
+        //we made the car with their parameters
+        //then we call completion to return to the user
+        val car = Car("Tesla", "ModelX", "Blue")
+        completion(car)
+
+    }
+
+    downloadCarData("fakeUrl.com") {car ->
+        println(car.color)
+        println(car.make)
+        println(car.model)
+    }
+
+    //the same
+    //lambda with only one argument can be placed with "it"
+    downloadCarData("fakeUrl.com"){
+        println(it.color)
+        println(it.make)
+        println(it.model)
+    }
+
+    fun downloadTruckData (url: String, completion : (Truck?, Boolean) -> Unit) {
+        //we make the web request
+        //we get the results back
+        val webRequestSuccess = true
+        if(webRequestSuccess){
+          val truck = Truck("Ford", "F-150", 10000)
+          completion(truck, true)
+        } else{
+            completion(null, false)
+        }
+    }
+
+    downloadTruckData("fakeUrl.com"){ truck, success ->
+        if (success){
+            truck?.accelerate()
+        } else{
+            println("Something went wrong!!")
+        }
+    }
+
+
 }
